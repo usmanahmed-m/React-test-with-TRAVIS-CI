44192c4cd4380501dd77cbb74d2f156eb744d04f
usmanahmed-m <usman-malik@outlook.com>
2020-11-01 23:29:06 +0500

    Created Project with a basic test and .travis.yml file

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,14 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("Hello World", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Hello World/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test("Hello World", () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Test Driven Development/i);
   expect(linkElement).toBeInTheDocument();
 });
